821c15ce0fefb89836b2a226648aa7d16c56ac27
caoscott <scottcao@outlook.com>
2016-09-09 23:27:31 -0500

    Add files via upload

---
+++ android/src/com/gamedeuce/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamedeuce.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamedeuce.game.Deuce;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Deuce(), config);
+	}
+}
