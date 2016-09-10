f3218c361f3888c4d42563c37f5c788ef6d4cc90
caoscott <scottcao@outlook.com>
2016-09-09 23:20:50 -0500

    Added libgdx library

---
+++ desktop/src/com/gamedeuce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamedeuce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamedeuce.game.Deuce;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Deuce(), config);
+	}
+}
